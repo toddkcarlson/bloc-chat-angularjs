087a8ad0cf353550ad9b0b4af5cb4363e01862e7
Todd Carlson <tcarlson@carlsonsw.com>
2018-01-22 22:13:02 -0500

    room list w/ ng-repeat

--- app/scripts/factories/Room.js
+++ app/scripts/factories/Room.js
@@ -6,6 +6,14 @@
 
     Room.all = rooms;
 
+    Room.add = function(room) {
+          rooms.$add({ foo: "bar" }).then(function(ref) {
+            var id = ref.key;
+            console.log("added record with id " + id);
+            rooms.$indexFor(id); // returns location in the array
+          });
+    } 
+
     return Room;
   }
 
